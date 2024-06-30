7f9684394e4e07b9a9b64045bc18feea72e2e59c
harisangar2004 <apharisangar4@gmail.com>
2024-06-30 17:07:29 +0530

    compress request and rate limit endpoints

--- server.js
+++ server.js
@@ -3,10 +3,19 @@ import express from 'express';
 import mainRoutes from './src/main.routes.js'; 
 import userRoutes from './src/user.routes.js'
 import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
+import compression from 'compression';
 
 const app = express();
 const port = 3000;
 
+const limiter = rateLimit({
+    windowMs: 60*100,
+    max:100
+})
+
+app.use(compression())
+app.use(limiter);
 app.use(express.json())
 app.use(helmet())
 
