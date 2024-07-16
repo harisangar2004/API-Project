b74701ff63b05671acc05a5416538784e76b6bb1
harisangar2004 <apharisangar4@gmail.com>
2024-07-16 19:28:46 +0530

    Fix error message while impossible to connect to server

--- src/components/user/UsersList.jsx
+++ src/components/user/UsersList.jsx
@@ -3,13 +3,23 @@ import { Button, Card, Col, Row } from 'react-bootstrap';
 import Layout from "../layout/Layout";
 import * as userService from "../../services/user.service.js";
 import { NavLink } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const UsersList = () => {
     const [users, setUsers] = useState({});
-
+    const [errorMessage, setErrorMessage] = useState(null);
     const fetchUsers = async () => {
-        const response = await userService.retrieveAllUser();
-        setUsers(response);
+        try {
+            const response = await userService.retrieveAllUser();
+            setUsers(response);
+        } catch (error) {
+            const retrieveErrorMessage = () => {
+                const apiErrorMessage = error?.response?.data?.message;
+                // Null Coalescing Operator
+                return apiErrorMessage ?? 'Error while connecting to the server';
+            }
+            setErrorMessage(retrieveErrorMessage());
+        }
     };
 
     useEffect(() => {
@@ -18,27 +28,35 @@ const UsersList = () => {
 
     return (
         <Layout >
-            <h3 className="text-center mb-5 ">Users</h3>
-            {Object.values(users).map(user => (
-                <Row key={user.id} className="justify-content-center">
-                    <Col>
-                        <Card >
-                            <Card.Body >
-                                <h4>{user.name}</h4>
-                                <p>{user.email}</p>
-                                {user.city && user.country && (
-                                    <p>
-                                        {user.city} - {user.country}
-                                    </p>
-                                )}
-                                <Button variant="primary" as={NavLink} to={`/edit/${user.id}`}>
-                                    Edit
-                                </Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                </Row>
-            ))}
+            {errorMessage ? (
+                <h4 className="text-center text-danger fw-bold">{errorMessage}</h4>
+            ) : (
+                <>
+                    <h3 className="text-center mb-2 ">Users</h3>
+                    {
+                        Object.values(users).map(user => (
+                            <Row key={user.id} className="justify-content-center">
+                                <Col>
+                                    <Card >
+                                        <Card.Body >
+                                            <h4>{user.name}</h4>
+                                            <h4>{user.email}</h4>
+                                            {user.city && user.country && (
+                                                <h4>
+                                                    {user.city} - {user.country}
+                                                </h4>
+                                            )}
+                                            <Button variant="primary" as={NavLink} to={`/edit/${user.id}`}>
+                                                Edit
+                                            </Button>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                            </Row>
+                        ))
+                    }
+                </>
+            )}
         </Layout>
     )
 }
